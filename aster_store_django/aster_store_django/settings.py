5d3b843106e15fc498f569796ab5fda7c1c8cd0c
Obioma29 <obiomachinwuko@yahoo.com>
2024-02-22 18:32:31 +0100

    Ecommerce website

--- aster_store_django/aster_store_django/settings.py
+++ aster_store_django/aster_store_django/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'corsheaders',
     'djoser',
+
+    'product',
 ]
 
 CORS_ALLOWED_ORIGINS = [
@@ -126,9 +128,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media/'
